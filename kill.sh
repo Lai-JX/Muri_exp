62a1c865f38e9be5db90aa21c510acdf00ae9980
Lai-JX <2840026059@qq.com>
2023-09-10 16:21:37 +0000

    update

--- kill.sh
+++ kill.sh
@@ -4,6 +4,7 @@ kill_all()
     ID=`ps | grep "$1" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`  # -v表示反过滤，awk表示按空格或tab键拆分，{print $2}表示打印第二个（这里对应进程号）
     for id in $ID 
     do  
+    echo "killed $id"  
     kill -9 $id  
     # echo "killed $id"  
     done
